f119913029c106c01d621bf3d8d11086e9700a56
Kristin <kbekk2@gmail.com>
2020-08-20 17:49:36 -0500

    Updated Access Token

--- app/services/access_token.rb
+++ app/services/access_token.rb
@@ -6,5 +6,7 @@ class AccessToken
         @model=model
     end
 
-    def generate(input)
-        
+    def generate(obt="")
+        Digest::SHA1.hexdigest("#({@model.to_s}-#{@model.created_at}-[base_api-#{@model.id.to_s(36)}]-[#(obj}]-[#{rand(-200..200).days.ago}]")
+    end
+end
